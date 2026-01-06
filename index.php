0c76733dd79abcc4c8e0398c5276e1402b5c7168
sougata <sougataghar47@gmail.com>
2026-01-06 15:21:45 +0530

    added home page

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
-echo 'Hello World';
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>PHP App</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://cdn.datatables.net/1.13.8/css/dataTables.bootstrap5.min.css" rel="stylesheet">
+</head>
+
+<body>
+
+  <?php
+    $html ="Welcome to Your Attendance Page";
+    include "./container.php";
+  ?>
+<script src="/js/main.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
