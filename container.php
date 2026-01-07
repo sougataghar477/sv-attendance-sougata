6006af570086991ba214f05484c7f50e37f86c70
sougata <sougataghar47@gmail.com>
2026-01-07 14:21:56 +0530

    added underline for active links in navbar with for loop

--- container.php
+++ container.php
@@ -10,7 +10,7 @@ $links = ["home", "attendance", "admin", "login", "register"];
 <div class="container">
   <nav class="navbar navbar-expand-lg navbar-light bg-light my-4">
     <div class="container-fluid">
-      <a class="navbar-brand" href="/">SV Infotech</a>
+      <a class="navbar-brand" href="/"><?php echo $isActiveLink?></a>
 
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
         <span class="navbar-toggler-icon"></span>
@@ -20,33 +20,24 @@ $links = ["home", "attendance", "admin", "login", "register"];
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <?php
           foreach ($links as $link) {
-
-              // hide admin if not admin
-              if ($link === "admin" && !$isAdminLoggedIn) continue;
-
-              // hide login & register if logged in
-              if (($link === "login" || $link === "register") && $isLoggedIn) continue;
-
-              // determine href
-              $href = ($link === "home") ? '/' : '/' . $link;
-
-              // active class
-              $isActive = (
-                  $currentPath === $href ||
-                  rtrim($currentPath, '/') === $href
-              )
-              ? ' underline'
-              : '';
-
-
-              echo '
-              <li class="nav-item">
-                <a class="nav-link' . $isActive . '" href="' . $href . '">
-                  ' . ucfirst($link) . '
+            $isActiveLink = $link === trim($currentPath,'/');
+            $activeClass= $isActiveLink ?'underline':'';
+              if(!$isAdminLoggedIn && $link=== 'admin'){
+                continue;
+              }
+              if($isLoggedIn && $link=== "login"){
+                continue;
+              }
+              if($isLoggedIn && $link=== "register"){
+                continue;
+              }
+              echo '<li class="nav-item">
+                <a class="nav-link '.$activeClass.'" href="'.'/'.$link.'">'
+                  . ucfirst($link) . '
                 </a>
               </li>';
-          }
-          ?>
+              
+          }?>
         </ul>
 
         <?php if ($isLoggedIn): ?>
